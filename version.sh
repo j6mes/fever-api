15602de46e62e16512cd39a5c2d62d23a8dcca77
James Thorne <james@jthorne.co.uk>
2019-03-18 17:55:44 +0000

    flask version

--- version.sh
+++ version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export VERSION_NUMBER=$(echo $TRAVIS_BRANCH | cut -d'v' -f 2).$TRAVIS_BUILD_NUMBER
 sed -i -e 's/0\.0\.0/'"$VERSION_NUMBER"'/g' setup.py
+sed -i -e 's/master\./0.0./g' setup.py
 echo "VERSION " $VERSION_NUMBER

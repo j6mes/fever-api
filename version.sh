cc9424aae2a79e5ceb89aba205cb87e3ac7ceff1
James Thorne <james@jthorne.co.uk>
2019-03-18 18:03:10 +0000

    assign version number before deploy rather than before build

--- version.sh
+++ version.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export VERSION_NUMBER=$(echo $TRAVIS_BRANCH | cut -d'v' -f 2).$TRAVIS_BUILD_NUMBER
 sed -i -e 's/0\.0\.0/'"$VERSION_NUMBER"'/g' setup.py
-sed -i -e 's/master\./0.0./g' setup.py
 echo "VERSION " $VERSION_NUMBER
